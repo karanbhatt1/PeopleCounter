bf3b3441f6788827a46c53abe6677ff4019fc902
karan bhatt <119851759+karanbhatt1@users.noreply.github.com>
2025-01-11 14:28:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+//document.getElementById("count-el").innerText=5
+let count=0
+let welcomeEl = document.getElementById("welcome-el")
+let name = "Karan Bhatt"
+let greeting = "Welcome back "
+welcomeEl.innerText = greeting + name
+welcomeEl.innerText = welcomeEl.innerText + " 🙏" //short hand operators can also be user;
+// console.log(count) // this is generally used for debugging purposes.
+let countEl = document.getElementById("count-el")
+
+function increment(){
+    count += 1
+    countEl.innerText = count
+}
+let totalCount= document.getElementById("totalCount")
+function save(){
+    totalCount.textContent += count   + " - "
+}
+
+ 
